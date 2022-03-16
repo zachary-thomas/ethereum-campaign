c7f3ae156aab6b8f2860a69c52de111c6b3b35b6
Zac <thomazac@mail.gvsu.edu>
2022-03-16 16:22:06 -0500

    add contribute function

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,9 +3,18 @@ pragma solidity ^0.8.9;
 contract Campaign {
     address public manager;
     uint public minimumContribution;
+    address[] public approvers;
+    
 
-    Campaign(uint minimum){
+    constructor (uint minimum){
         manager = msg.sender;
         minimumContribution = minimum;
     }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers.push(msg.sender);
+    }
+
 }

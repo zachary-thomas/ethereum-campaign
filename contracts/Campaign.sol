34eeec9709bca0167f37f8f8ac62784f5e72a1da
Zac <thomazac@mail.gvsu.edu>
2022-03-24 16:55:07 -0500

    add in struct type and createRequest

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,10 +1,24 @@
 pragma solidity ^0.8.9;
 
 contract Campaign {
+
+    // Struct type
+    struct Request{
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
-    
+
+    modifier restricted(){
+        require(msg.sender == manager);
+        _;
+    }
 
     constructor (uint minimum){
         manager = msg.sender;
@@ -15,6 +29,18 @@ contract Campaign {
         require(msg.value > minimumContribution);
 
         approvers.push(msg.sender);
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        
+        Request newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+
+        requests.push(newRequest);
     }
 
 }
